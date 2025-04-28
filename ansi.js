72d08c0b8e5a42c3854c61ce5320bae01a5c9261
Shea <nu@she-a.eu>
2025-04-29 00:05:13 +0300

    added simple ansi

--- ansi.js
+++ ansi.js
@@ -20,7 +20,6 @@ arr = Array.from({ length: sizeY }, () => Array(sizeX).fill(0));
 async function setupGrid() {
     let html = "";
     
-    // Create grid items
     for (let i = 0; i < sizeY; i++) {
         for (let j = 0; j < sizeX; j++) {
             html += "<div id=\"C"+i+","+j+"\" class=\"grid-cell\"></div>";
@@ -52,6 +51,8 @@ async function setupGrid() {
         }
     }
 }
+		ansible.innerHTML += "<span>arara</span>";
+
 
 
 function containsAlphabet(str) {
@@ -63,7 +64,7 @@ function printer(posX, posY, colbg, colfg, oup){
         let x = Math.floor(posX)+i-1;
         let y = Math.floor(posY)-1;
         if (x < 0 || x > sizeX || y < 0 || y > sizeY){ 
-			console.log({"posX": posX, "posY": posY}, {"x": x, "y": y});
+//			console.log({"posX": posX, "posY": posY}, {"x": x, "y": y});
 			continue;
 		}
         let obj = document.getElementById("C" + y + "," + x);
@@ -77,14 +78,25 @@ function printer(posX, posY, colbg, colfg, oup){
     }
 }
 
-/*
-/*
-i = 0;
-while(i<100){
-	printer(i*20, 10, defbg, deffg, "█▀");
-	i += 1;
+function ansiTable(inp){
+	if(inp.includes("30")||inp.includes( "40")) return "rgb(40,40,40)";	//Black (dark)
+	if(inp.includes("31")||inp.includes( "41")) return "#cc241d";		//Red
+	if(inp.includes("32")||inp.includes( "42")) return "#00ff00";		//Green
+	if(inp.includes("33")||inp.includes( "43")) return "rgb(255,255,0)";//Yellow
+	if(inp.includes("34")||inp.includes( "44")) return "#458588"; 		//Blue
+	if(inp.includes("35")||inp.includes( "45")) return "Magenta"; 		//Magenta
+	if(inp.includes("36")||inp.includes( "46")) return "Cyan";			//Cyan
+	if(inp.includes("37")||inp.includes( "47")) return "Gray";			//White (dark)
+	if(inp.includes("90")||inp.includes("100")) return "DimGray";		//Black	(light)
+	if(inp.includes("91")||inp.includes("101")) return "#fb4934";		//red
+	if(inp.includes("92")||inp.includes("102")) return "Lime";			//green
+	if(inp.includes("93")||inp.includes("103")) return "#fabd2f";		//Yellow
+	if(inp.includes("94")||inp.includes("104")) return "#89b4fa";		//Blue
+	if(inp.includes("95")||inp.includes("105")) return "#f38ba8";		//Magenta
+	if(inp.includes("96")||inp.includes("106")) return "Cyan";			//Cyan
+	if(inp.includes("97")||inp.includes("107")) return "#ffffff";		//White (light)
 }
-*/
+
 fetch('test.txt')
   .then(response => response.text())
   .then(async text => {
@@ -109,28 +121,42 @@ fetch('test.txt')
 				if(posX == 0) posX = 1;
 			}
 			else if(tokens[i].includes("[2J")){
-				tally = 0;
+				
 			}
 			else if(tokens[i][0] == '['){
-				if(tokens[i].startsWith("[38;2;")){
-					tokens[i] = tokens[i].substring(6);
-					colfg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
-					colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
-					colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+')';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf(';'));
+				if(tokens[i].includes("[38;2;") || tokens[i].includes(";48;2;")){
+					if(tokens[i].startsWith("[38;2;")){
+						tokens[i] = tokens[i].substring(6);
+						colfg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+						colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+						colfg+= tokens[i].substring(0, tokens[i].indexOf(';'))+')';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf(';'));
+					}
+					if(tokens[i].startsWith(";48;2;")){
+						tokens[i] = tokens[i].substring(6);
+						colbg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+						colbg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
+						colbg+= tokens[i].substring(0, tokens[i].indexOf('m'))+')';
+						tokens[i] = tokens[i].substring(tokens[i].indexOf('m')+1);
+					}
 				}
-				if(tokens[i].startsWith(";48;2;")){
-					tokens[i] = tokens[i].substring(6);
-					colbg = "rgb("+tokens[i].substring(0, tokens[i].indexOf(';'))+',';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
-					colbg+= tokens[i].substring(0, tokens[i].indexOf(';'))+',';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf(';')+1);
-					colbg+= tokens[i].substring(0, tokens[i].indexOf('m'))+')';
-					tokens[i] = tokens[i].substring(tokens[i].indexOf('m')+1);
+				else{
+					if(tokens[i].substring(1,3) >= 100 || (tokens[i].substring(1,3) >= 40 &&tokens[i].substring(1,3) <= 50))
+						colbg = ansiTable(tokens[i].substring(1,3));
+					else
+						colfg = ansiTable(tokens[i].substring(1,3));
+					if(tokens[i].substring(4,7) >= 100 || (tokens[i].substring(4,7) >= 40 &&tokens[i].substring(4,7) <= 50))
+						colbg = ansiTable(tokens[i].substring(4,7));
+					else
+						colbg = ansiTable(tokens[i].substring(4,7));
+					tokens[i] = tokens[i].substring(7);
+					if(tokens[i][0]=="m" && tokens[i].length < 3)
+						tokens[i] = tokens[i].substring(1);
 				}
-				//console.log(colbg,tokens[i]);
 				printer(posX,posY, colbg, colfg, tokens[i]);
 			}
 			else{
@@ -138,6 +164,5 @@ fetch('test.txt')
 			}
 			i++;
 		}
-		//ansible.innerHTML=(text);
 	})
 
