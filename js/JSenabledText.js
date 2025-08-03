44b5bdeaaf9951fec2b7bc9e66287554811823b8
Shea Duma <nu@she-a.eu>
2025-08-03 04:52:42 +0300

    corrected favicon and text

--- js/JSenabledText.js
+++ js/JSenabledText.js
@@ -1,3 +1,2 @@
 Desc = document.getElementById("Desc");
 Desc.innerHTML += "<p> Hi, this is shea, this is my landing page for all my projects, please see below the one i find the most satisfying, hope you are here for a good reason and not because i pissed you off on BSKY or anything of that sort.</p>";
-Desc.innerHTML += "<p> Ok but srs NYA!</p>";
